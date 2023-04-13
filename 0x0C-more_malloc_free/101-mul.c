a3add02e121c86aa2d3df1b5fcd1be023bfa8112
Dev-Japhyl <jegedejaphet123@gmail.com>
2023-04-13 15:42:12 -0700

    More malloc, free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -13,9 +13,11 @@ int main(int argc, char *argv[])
 {
 	unsigned long mul;
 	int i, j;
+
 		if (argc != 3)
 		{ printf("Error\n");
 		exit(98); }
+
 		for (i = 1; i < argc; i++)
 		{
 			for (j = 0; argv[i][j] != '\0'; j++)
@@ -24,7 +26,6 @@ int main(int argc, char *argv[])
 				{  printf("Error\n");
 				exit(98); }
 			}
-
 		}
 	mul = atol(argv[1]) *  atol(argv[2]);
 	printf("%lu\n", mul);
